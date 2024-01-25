6d24f3650a5de22b8b4e4f67cdf4c0f304ec5eaa
strategy <ojobalaro2018@gmail.com>
2024-01-25 04:02:53 -0800

    make changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,94 @@
-import React from "react";
-import {Route, Routes } from "react-router-dom";
-import Dashboard from "./Dashboard";
-import Info from "./Info";
+import React, { useState } from "react";
+import { useEffect } from "react";
+
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/info" element={<Info />} />
-      </Routes>
+      <Drug>
+        <DrugList />
+      </Drug>
+      <DrugContent />
     </div>
   )
 
 }
 
+
 export default App
+
+const Drug = ({ children }) => {
+
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+const DrugList = () => {
+  const [query, setQuery] = useState('')
+  const [drugs, setDrugs] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const [iserror, setError] = useState('')
+
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value)
+  }
+
+  useEffect(() => {
+
+    async function fetchDrugs() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`https://api.fda.gov/drug/drugsfda.json?api_key=MgktfyXuOpmmSw3nmZmq2oq5rdeCLTFAA3CLbqSM&search=openfda.brand_name:"${query}"&limit=5`)
+        if (!res.ok) 
+        throw new Error("Something went wrong")
+        const data = await res.json()
+        let result = data.results
+        
+        const brandGenericName = Object.entries(result).map((fda) => fda.at(1).openfda.brand_name)
+        setDrugs(prevBrandGenericName => {
+          return [...prevBrandGenericName, ...brandGenericName]
+        })
+        
+      } catch (err) {
+        setError(err.message)
+      }finally{
+        setIsLoading(false)
+      }
+    }
+    if (!query) return
+
+    fetchDrugs()
+  }, [query])
+  return (
+    <div>
+      <input value={query} onChange={handleQuery} />
+      {isLoading && <Loading />}
+      {!isLoading && !iserror && drugs}
+
+      {iserror && <ErrorMessage msg={iserror} />}
+
+    </div>
+  )
+}
+
+const DrugContent = () => {
+  return (
+    <div></div>
+  )
+}
+
+const Loading = () => {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+const ErrorMessage = ({msg}) => {
+  return (
+    <p>{msg}</p>
+  )
+}
--- src/drugsearch.js
+++ src/drugsearch.js
@@ -1,83 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
 
-const drugsearch = (query) => {
+// const drugsearch = (query) => {
 
-    const [brand, setBrand] = useState([])
-    const [dosage, setDosage] = useState([])
-    const [pharmClass, setpharmClass] = useState([])
-    const [geneName, setGeneName] = useState([])
-    const [active, setActive] = useState([])
-    const [route, setRoute] = useState([])
-    const [prodType, setProdtype] = useState([])
-    const [pack, setPack] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
+//     const [brand, setBrand] = useState([])
+//     const [dosage, setDosage] = useState([])
+//     const [pharmClass, setpharmClass] = useState([])
+//     const [geneName, setGeneName] = useState([])
+//     const [active, setActive] = useState([])
+//     const [route, setRoute] = useState([])
+//     const [prodType, setProdtype] = useState([])
+//     const [pack, setPack] = useState([])
+//     const [loading, setLoading] = useState(true)
+//     const [error, setError] = useState(false)
 
-    useEffect(() => {
-        setBrand([])
-        setDosage([])
-        setGeneName([])
-        setpharmClass([])
-        setRoute([])
-        setProdtype([])
-        setActive([])
-        setPack([])
-        setError(() => { return false })
-    }, [query])
+//     useEffect(() => {
+//         setBrand([])
+//         setDosage([])
+//         setGeneName([])
+//         setpharmClass([])
+//         setRoute([])
+//         setProdtype([])
+//         setActive([])
+//         setPack([])
+//         setError(() => { return false })
+//     }, [query, setBrand([]), setDosage([])])
 
-    useEffect(() => {
-        setLoading(true)
-        let Cancel
-        axios({
-            method: 'GET',
-            url: "https://api.fda.gov/drug/ndc.json?limit=1",
-            params: { search: query },
-            cancelToken: new axios.CancelToken(c => Cancel = c)
-        }).then(res => {
+//     useEffect(() => {
+//         setLoading(true)
+//         let Cancel
+//         axios({
+//             method: 'GET',
+//             url: "https://api.fda.gov/drug/ndc.json?limit=1",
+//             params: { search: query },
+//             cancelToken: new axios.CancelToken(c => Cancel = c)
+//         }).then(res => {
 
-            setGeneName(prevGeneName => {
-                return [...prevGeneName, ...res.data.results.map((resut => resut.generic_name))]
-            })
+//             setGeneName(prevGeneName => {
+//                 return [...prevGeneName, ...res.data.results.map((resut => resut.generic_name))]
+//             })
 
-            setBrand(prevBrand => {
-                return [...prevBrand, ...res.data.results.map(result => result.brand_name)]
-            })
-            setDosage(prevDose => {
-                return [...prevDose, ...res.data.results.map(result => result.dosage_form)]
-            })
+//             setBrand(prevBrand => {
+//                 return [...prevBrand, ...res.data.results.map(result => result.brand_name)]
+//             })
+//             setDosage(prevDose => {
+//                 return [...prevDose, ...res.data.results.map(result => result.dosage_form)]
+//             })
 
-            setpharmClass(prevClass => {
-                return [...prevClass, ...res.data.results.map(result => result.pharm_class)]
-            })
+//             setpharmClass(prevClass => {
+//                 return [...prevClass, ...res.data.results.map(result => result.pharm_class)]
+//             })
 
-            setRoute(prevRoute => {
-                return [...prevRoute, ...res.data.results.map(result => result.route)]
-            })
+//             setRoute(prevRoute => {
+//                 return [...prevRoute, ...res.data.results.map(result => result.route)]
+//             })
 
-            setProdtype(prodType => {
-                return [...prodType, ...res.data.results.map(result => result.product_type)]
-            })
+//             setProdtype(prodType => {
+//                 return [...prodType, ...res.data.results.map(result => result.product_type)]
+//             })
 
-            setActive(prevActive => {
-              return[ ...prevActive, ...res.data.results.map(result => result.active_ingredients)]
-            })
+//             setActive(prevActive => {
+//               return[ ...prevActive, ...res.data.results.map(result => result.active_ingredients)]
+//             })
 
-            setPack(prevpack => {
-                return[ ...prevpack, ...res.data.results.map(result => result.packaging)]
-              })
+//             setPack(prevpack => {
+//                 return[ ...prevpack, ...res.data.results.map(result => result.packaging)]
+//               })
 
-            console.log(res.data)
-            setLoading(false)
-        }).catch(error => {
-            if (axios.isCancel(error)) return
-            setError(true)
-            setLoading(false)
-        })
+//             console.log(res.data)
+//             setLoading(false)
+//         }).catch(error => {
+//             if (axios.isCancel(error)) return
+//             setError(true)
+//             setLoading(false)
+//         })
 
-        return () => Cancel()
-    }, [query])
-    return { brand, loading, error, dosage, geneName, pharmClass, route, active, prodType, pack }
-}
+//         return () => Cancel()
+//     }, [query])
+//     return { brand, loading, error, dosage, geneName, pharmClass, route, active, prodType, pack }
+// }
 
 export default drugsearch
